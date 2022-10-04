87d5fd77b95c2b4240dacb339f08fe891fca42bf
task11 <task11@naver.com>
2022-10-04 23:45:21 +0900

    feat: 기본 Router 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { ThemeProvider } from 'styled-components';
-
 import { defaultTheme, GlobalStyle } from './style';
+
+import AppRouter from './Router';
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <div>
-        App
-      </div>
+      <AppRouter />
     </ThemeProvider>
   );
 }
