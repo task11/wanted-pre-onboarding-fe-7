5f1bd7de1a8d85d227475fcc420f7bd11c2e7d19
task11 <task11@naver.com>
2022-10-05 16:44:22 +0900

    feat: Login/Register form 구현, input validation 구현

--- src/Auth/AccessType.js
+++
@@ -1,4 +0,0 @@
-export const ACCESS_TYPE = {
-  'LOGIN': '로그인',
-  'REGISTER': '회원가입',
-};
---
+++ src/Auth/AuthForm.jsx
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+
+import { useEffect, useState } from 'react';
+
+import { ACCESS_TYPE, initialUserInfo } from '../constants';
+
+import {
+  validationUserLogin,
+  validationUserRegister
+} from '../utils/functions';
+
+import AuthButton from '../components/Auth/AuthButton';
+import AuthContent from "../components/Auth/AuthContent";
+import InputWithLabel from "../components/Auth/InputWithLabel";
+
+export default function AuthForm({ accessType }) {
+  const [userInfo, setUserInfo] = useState(initialUserInfo);
+
+  useEffect(() => {
+    setUserInfo(initialUserInfo);
+  }, [accessType]);
+
+  const handleChange = ({ target: { value, name } }) => {
+    setUserInfo((currentState) => {
+      return {
+        ...currentState,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleButtonDisabled = () => {
+    const { email, password, passwordCheck } = userInfo;
+
+    if (accessType === ACCESS_TYPE.LOGIN) {
+      return validationUserLogin(email, password);
+    } else if (accessType === ACCESS_TYPE.REGISTER) {
+      return validationUserRegister(email, password, passwordCheck);
+    }
+
+    return true;
+  };
+
+  const handleSubmit = () => {
+    const { email, password, passwordCheck } = userInfo;
+  };
+
+  return (
+    <AuthContent title={accessType}>
+      <StyledInputWrapper>
+        <InputWithLabel
+          label="이메일"
+          name="email"
+          value={userInfo.email}
+          onChange={(e) => handleChange(e)}
+          placeholder="이메일"
+        />
+        <InputWithLabel
+          label="비밀번호"
+          name="password"
+          value={userInfo.password}
+          onChange={(e) => handleChange(e)}
+          placeholder="비밀번호"
+          type="password"
+        />
+        {
+          accessType === ACCESS_TYPE.REGISTER &&
+          <InputWithLabel
+            label="비밀번호 확인"
+            name="passwordCheck"
+            value={userInfo.passwordCheck}
+            onChange={(e) => handleChange(e)}
+            placeholder="비밀번호 확인"
+            type="password"
+          />
+        }
+        <AuthButton
+          onClick={() => handleSubmit()}
+          disabled={handleButtonDisabled()}
+        >
+          {accessType}
+        </AuthButton>
+      </StyledInputWrapper>
+    </AuthContent>
+  );
+};
+
+const StyledInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
