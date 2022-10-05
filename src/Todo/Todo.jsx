4b1353399c803bd27349afcf97fba03495a468ff
task11 <task11@naver.com>
2022-10-05 21:56:43 +0900

    feat: Todo 컴포넌트 구현

---
+++ src/Todo/Todo.jsx
@@ -0,0 +1,112 @@
+import styled from 'styled-components';
+
+import { useEffect, useState } from 'react';
+
+import { axios } from '../lib/axios';
+
+import TodoWrapper from '../components/Todo/TodoWrapper';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
+export default function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [inputTodo, setInputTodo] = useState('');
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = async () => {
+    try {
+      const result = await axios.get('/todos');
+      setTodos(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = ({ target: { value } }) => {
+    setInputTodo(value);
+  };
+
+  const handleSubmitTodo = async () => {
+    try {
+      const result = await axios.post('/todos', {
+        todo: inputTodo,
+      });
+
+      setTodos((currentState) => {
+        return [
+          ...currentState,
+          result
+        ];
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateTodo = async (todoId, newTodo, isCompleted) => {
+    try {
+      const result = await axios.put(`/todos/${todoId}`, {
+        todo: newTodo,
+        isCompleted: isCompleted
+      });
+
+      setTodos((currentState) => {
+        const newTodos = [...currentState];
+        const index = newTodos.findIndex((todo) => todoId === todo.id);
+
+        newTodos[index] = result;
+
+        return newTodos;
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteTodo = async (todoId) => {
+    try {
+      if (!window.confirm('정말로 삭제하시겠습니까?')) {
+        return;
+      }
+
+      await axios.delete(`/todos/${todoId}`);
+
+      setTodos((currentState) => {
+        return currentState.filter(({ id }) => id !== todoId);
+      });
+
+      alert('삭제가 완료되었습니다.');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <StyledTodoForm>
+      <TodoWrapper>
+        <TodoForm
+          inputTodo={inputTodo}
+          handleChange={handleChange}
+          handleSubmitTodo={handleSubmitTodo}
+        />
+        <TodoList
+          todos={todos}
+          updateTodo={updateTodo}
+          deleteTodo={deleteTodo}
+        />
+      </TodoWrapper>
+    </StyledTodoForm>
+  );
+};
+
+const StyledTodoForm = styled.div`
+  display: flex;
+  background-color: white;
+  justify-content: center;
+  width: 600px;
+  height: 500px;
+  border-radius: 20px;
+`;
