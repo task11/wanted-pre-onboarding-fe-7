f56642a05ef8d57e38eddecfe4b2ba646d70db5b
task11 <task11@naver.com>
2022-10-06 14:24:36 +0900

    fix: release 버그 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,7 +7,7 @@ import Todo from './Todo/Todo';
 
 export default function AppRouter() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route
           path="/todo"
